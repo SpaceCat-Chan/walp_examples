2e4fcf43b91ea32408ca32436423d30b89bca77c
SpaceCat-Chan <49094338+SpaceCat-Chan@users.noreply.github.com>
2022-11-10 22:28:42 +0100

    more extensive tests

--- tests/run.lua
+++ tests/run.lua
@@ -19,16 +19,125 @@ module.IMPORTS = {
 
 walp.instantiate(module)
 
-module.EXPORTS.memory.write64(1, { l = 0x08040201, h = 0x80402010 })
-local loaded = module.EXPORTS.i64_load.call(true, 1)
-print(string.format("loaded: 0x%X", loaded))
-assert(loaded == 0x8040201008040201ULL, "i64_load failed")
-print("i64_load succeded")
+local function run_tests(tests)
+    local passed_tests = {}
+    local passed_test_count = 0
+    local failed_tests = {}
+    local ignored_test_count = 0
+    for _, test in ipairs(tests) do
+        local should_do_test = true
+        if test.relies_on then
+            for _, must_pass in pairs(test.relies_on) do
+                should_do_test = should_do_test and passed_tests[must_pass] ~= nil
+            end
+        end
+        if should_do_test then
+            local success, message = test.run()
+            if success then
+                passed_tests[test.name] = true
+                passed_test_count = passed_test_count + 1
+            else
+                table.insert(failed_tests, { test = test, message = message })
+            end
+        else
+            ignored_test_count = ignored_test_count + 1
+        end
+    end
+    if ignored_test_count == 1 then
+        print("skipped 1 test")
+    else
+        print(string.format("skipped %i tests", ignored_test_count))
+    end
+    if passed_test_count == 1 then
+        print("passed 1 test")
+    else
+        print(string.format("passed %i tests", passed_test_count))
+    end
+    if #failed_tests == 1 then
+        print("failed 1 test")
+    else
+        print(string.format("failed %i tests", #failed_tests))
+    end
+    for _, test in pairs(failed_tests) do
+        print(("failed test %s: %s"):format(test.test.name, test.message))
+    end
+end
 
--- i64.load can now be trusted
+local tests = {
+    {
+        name = "local.get",
+        run = function()
+            local res = module.EXPORTS.local_get.call(true, 123456)
+            return res == 123456, ("got: %i, expected: 123456"):format(res)
+        end
+    },
+    {
+        name = "i32.const",
+        run = function()
+            local res = module.EXPORTS.i32_const.call(true)
+            return res == 143723548, ("got: %i, expected: 143723548"):format(res)
+        end
+    },
+    {
+        name = "i64.load",
+        relies_on = { "local.get" },
+        run = function()
+            module.EXPORTS.memory.write64(1, { l = 0x08040201, h = 0x80402010 })
+            local loaded = module.EXPORTS.i64_load.call(true, 1)
+            return loaded == 0x8040201008040201ULL,
+                string.format("loaded: 0x%X, expected: 0x8040201008040201", loaded)
+        end
+    },
+    {
+        name = "i64.store",
+        relies_on = { "i64.load" },
+        run = function()
+            module.EXPORTS.i64_store.call(true, 1, 0xFF7F3F1F0F070301ULL)
+            local stored = module.EXPORTS.i64_load.call(true, 1)
+            return stored == 0xFF7F3F1F0F070301ULL, string.format("stored: 0x%X, expected: 0xFF7F3F1F0F070301", stored)
+        end
+    },
+    {
+        name = "i32.add param",
+        relies_on = { "local.get" },
+        run = function()
+            local added = module.EXPORTS.i32_add_param.call(true, 654321, 77775)
+            return added == 654321 + 77775, string.format("got: %i, expected: 732096", added)
+        end
+    },
+    {
+        name = "i32.add const",
+        relies_on = { "i32.const" },
+        run = function()
+            local added = module.EXPORTS.i32_add_const.call(true)
+            return added == 143723548, string.format("got: %i, expected: 143723548", added)
+        end
+    },
+    {
+        name = "global.set",
+        relies_on = { "local.get" },
+        run = function()
+            module.EXPORTS.global_set.call(true, 74368345)
+            local val = module.EXPORTS.global.get()
+            return val == 74368345, ("global was set to %i, expected 74368345"):format(val)
+        end
+    },
+    {
+        name = "global.get",
+        relies_on = { "global.set" },
+        run = function()
+            module.EXPORTS.global_set.call(true, 65258)
+            local val = module.EXPORTS.global_get.call(true)
+            return val == 65258, ("got %i, expected 65258"):format(val)
+        end
+    },
+    {
+        name = "i64.const",
+        run = function()
+            local val = module.EXPORTS.i64_const.call(true)
+            return val == 0xFEDCBA9876543210ULL, ("got 0x%X, expected 0xFEDCBA9876543210"):format(val)
+        end
+    }
+}
 
-module.EXPORTS.i64_store.call(true, 1, 0xFF7F3F1F0F070301ULL)
-local stored = module.EXPORTS.i64_load.call(true, 1)
-print(string.format("stored: 0x%X", stored))
-assert(stored == 0xFF7F3F1F0F070301ULL, "i64_store failed")
-print("i64_store succeded")
+run_tests(tests)
--- tests/test.wat
+++ tests/test.wat
@@ -1,17 +1,50 @@
 (module
-  (type $t0 (func (param i32) (result i64)))
-  (func $i64_load (type $t0) (param $p0 i32) (result i64)
+  (func $i64_load (param $p0 i32) (result i64)
     local.get $p0
     i64.load
   )
-  (type $t1 (func (param i32) (param i64)))
-  (func $i64_store (type $t1) (param $p0 i32) (param $p1 i64)
+  (func $i64_store (param $p0 i32) (param $p1 i64)
     local.get $p0
     local.get $p1
     i64.store
   )
+  (func $local_get (param $p0 i32) (result i32)
+    local.get $p0
+  )
+  (func $i32_const (result i32)
+    i32.const 143723548
+  )
+  (func $i32_add_param (param $p0 i32) (param $p1 i32) (result i32)
+    local.get $p0
+    local.get $p1
+    i32.add
+  )
+  (func $i32_add_const (result i32)
+    i32.const 587179435
+    i32.const 3851511409
+    i32.add
+  )
+  (func $global_set (param $p0 i32)
+    local.get $p0
+    global.set $global
+  )
+  (func $global_get (result i32)
+    global.get $global
+  )
+  (func $i64_const (result i64)
+    i64.const 0xFEDCBA9876543210
+  )
   (export "i64_load" (func $i64_load))
   (export "i64_store" (func $i64_store))
+  (export "local_get" (func $local_get))
+  (export "i32_const" (func $i32_const))
+  (export "i32_add_param" (func $i32_add_param))
+  (export "i32_add_const" (func $i32_add_const))
+  (export "global_set" (func $global_set))
+  (export "global_get" (func $global_get))
+  (export "i64_const" (func $i64_const))
   (memory $memory 1)
   (export "memory" (memory 0))
+  (global $global (mut i32) (i32.const 0))
+  (export "global" (global $global))
 )
